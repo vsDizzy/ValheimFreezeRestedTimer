73ffa34c5c307ef4ae621bf2166bed38567b3d1d
Dizzy <todizzy@gmail.com>
2026-06-15 22:00:58 +0300

    chore: initial commit

---
+++ ValheimFreezeRestedTimer.cs
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 vsDizzy
+
+// #ref ${Managed}/assembly_valheim.dll
+// #ref ${Managed}/UnityEngine.CoreModule.dll
+
+using System;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+
+namespace Valheim
+{
+    [BepInPlugin("com.vortex.valheim.freezerestedtimer", "Valheim Freeze Rested Timer", "1.0.0")]
+    public class FreezeRestedTimerPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            FreezeRestedTimer.Main();
+            Logger.LogInfo("Freeze Rested Timer (DLL) initialized!");
+        }
+
+        private void OnDestroy()
+        {
+            FreezeRestedTimer.Unload();
+        }
+    }
+
+    public static class FreezeRestedTimer
+    {
+        private static Harmony harmony;
+
+        public static void Main()
+        {
+            if (harmony != null) return;
+            harmony = new Harmony("com.vortex.valheim.freezerestedtimer");
+            harmony.PatchAll(typeof(FreezeRestedTimer));
+        }
+
+        public static void Unload()
+        {
+            harmony?.UnpatchSelf();
+            harmony = null;
+        }
+
+        [HarmonyPatch(typeof(Player), "UpdateModifiers")]
+        [HarmonyPrefix]
+        public static void PrefixModifiers(Player __instance)
+        {
+            if (__instance == null) return;
+
+            var seman = __instance.GetSEMan();
+            if (seman == null) return;
+
+            var statusEffects = seman.GetStatusEffects();
+            if (statusEffects == null) return;
+
+            foreach (var statusEffect in statusEffects)
+            {
+                if (statusEffect != null && statusEffect.m_name == "$se_rested_name")
+                {
+                    statusEffect.m_time = 0f;
+                }
+            }
+        }
+    }
+}
